1e38b80dc2d35fb5392c90c9d32dceaacca16548
ValikShv <bobrshv@gmail.com>
2020-12-07 12:57:04 +0200

    Problem with email2

--- register_test_task/src/App.js
+++ register_test_task/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Component } from 'react';
+import firebase from 'firebase';
 
 // class App extends React.Component {
 //   render() {
@@ -8,14 +9,79 @@ import { Component } from 'react';
 // 
 
 export default class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  componentDidMount(){
+    const db = firebase.database();
+    // console.log(db);
+  }
+
+  handleChange = ({target: { value, id}})=>{
+    this.setState({
+      [id]: value,
+    })
+
+    // console.log(value, id);
+  };
+
+  createAccount = () => {
+    const {email, password} = this.state;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(response => {
+      this.setState({hasAccount: true})
+    }) 
+      .catch(error => console.log(error)); 
+      // для реестрации
+  };
+
+  // logInAccount = () => {
+  //   const {email, password} = this.state;
+  //   firebase.auth().signInWithEmailAndPassword(email, password)
+  //   .then(response => {
+  //     this.setState({hasAccount: true})
+  //   }) 
+  //   .catch(error => console.log(error));
+
+  //   // для авторизации
+  // };
+
   render() {
+    const { hasAccount } = this.state;
     return (
       <div>
-        <div className="login_block">
-          <input type="text" />
-          <input type="password" />
-          <input type="submit" />
-        </div>
+        {
+          hasAccount ?
+          (
+            <div>hello</div>
+          ):
+            (
+              <div className="login_block">
+              <input 
+              type="text" 
+              id="email" 
+              onChange={this.handleChange} 
+              />
+    
+              <input 
+              type="password" 
+              id="password"
+              onChange={this.handleChange}
+              />
+    
+              <input 
+              type="submit" 
+              onClick={this.createAccount}
+              />
+            </div>
+            )
+        } 
+        
       </div>
     )
   }
--- register_test_task/src/index.js
+++ register_test_task/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjiuzOFXCKtVJzWA0E8zmo1bWX0yQxJEc",
+    authDomain: "registertesttask.firebaseapp.com",
+    databaseURL: "https://registertesttask-default-rtdb.firebaseio.com",
+    projectId: "registertesttask",
+    storageBucket: "registertesttask.appspot.com",
+    messagingSenderId: "601364190131",
+    appId: "1:601364190131:web:f181213c834325603cf1c6",
+    measurementId: "G-W8JGCXZ4DK"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
